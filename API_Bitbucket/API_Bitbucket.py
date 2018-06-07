507b0753fe4f496947665fe9a440a831e43d5941
renjithcg <renjithcg09@gmail.com>
2018-06-07 18:19:36 +0530

    calling repository and clone URL usinf ForLoop

--- API_Bitbucket/API_Bitbucket.py
+++ API_Bitbucket/API_Bitbucket.py
@@ -4,15 +4,14 @@ import json
 
 API_URL='https://api.bitbucket.org/2.0/repositories/renjithcg'
 
-Result = requests.get(API_URL,headers={'content-type': 'application/json'},verify=False)
-if Result.status_code==200:
-    ResultJSON=json.loads(Result.text)
-    #print(ResultJSON)
-    s=0
-    while s<3:
-        print(ResultJSON['values'][s]['name'])
-        s=s+1
+Result = requests.get(API_URL)
 
-    print(ResultJSON['values'][0]['links']['clone'][-1])
-    print(ResultJSON['values'][1]['links']['clone'][-1])
-    print(ResultJSON['values'][2]['links']['clone'][-1])
+if Result.status_code==200:
+    ResultJSON = json.loads(Result.text)
+    for obj in ResultJSON['values']:
+        print(obj['name'])
+
+    for url in ResultJSON['values']:
+        print(url['links']['clone'][-1]['href'])
+
+   
